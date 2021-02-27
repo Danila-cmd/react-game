3049fa04076f62c22c5835c2a9574e970a33090b
Danila-cmd <dvfeduleev@gmail.com>
2021-02-28 01:54:22 +0500

    feat: add sound for mainMenu on play button

--- frontend/src/components/MainMenu.js
+++ frontend/src/components/MainMenu.js
@@ -2,9 +2,15 @@ import React from 'react'
 import {Link} from "react-router-dom";
 import "./mainMenu-style.css"
 
+import useSound from "use-sound";
+import sound from "./Sound/bum.mp3"
+
+
 const MainMenu = () => {
 
     const gamer = localStorage.getItem('gamer');
+
+    const [play] = useSound(sound)
 
     return (
         <div id="gameMenu">
@@ -20,7 +26,10 @@ const MainMenu = () => {
             ) : (
                 <div id="gameMenu">
                     <div className="buttonToPlay">
-                        <Link to="/game" className="btn btn-primary">ИГРАТЬ</Link>
+                        <Link to="/game"
+                              className="btn btn-primary"
+                              onClick={play}
+                        >ИГРАТЬ</Link>
                     </div>
                     <div className="buttonToPlay">
                         <Link to="/createGamer" className="btn btn-dark disabled">СОЗДАТЬ ИГРОКА</Link>
