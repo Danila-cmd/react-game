c31b01317a0a816a3b3f299920b9eedad75c5733
Danila-cmd <dvfeduleev@gmail.com>
2021-03-02 19:44:36 +0500

    feat: add sound on gameover

--- frontend/src/components/Game/componentsForGame/Cell.js
+++ frontend/src/components/Game/componentsForGame/Cell.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {StyledCell} from "./style/StyledCell";
+import {CellStyled} from "./style/CellStyled";
 import {TETROMINOS} from "../tetrisFigure.js";
 
 const Cell = ({type}) => {
     return (
-        <StyledCell type={type} color={TETROMINOS[type].color}/>
+        <CellStyled type={type} color={TETROMINOS[type].color}/>
     )
 }
 
